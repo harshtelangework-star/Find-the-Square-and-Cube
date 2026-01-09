04f1066fdca442b77cabd4fd4515d973b9d0666f
harshtelangework-star <harshtelangework@gmail.com>
2026-01-09 21:40:46 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,15 @@
+//Store a number in a variable and:
+
+//Print its square
+
+//Print its cube
+
+public class main {
+    public static void main(String[]args){
+      
+        int a = 20;
+        System.out.println(a*a);
+
+        System.out.println(a*a*a);
+    }
+}
